ab9c15708369125fded88c82cfe3737fd61e0023
w3schools-test <test@w3schools.com>
2023-08-17 13:42:30 +0545

    fastapi get method

---
+++ fast_api/bibek/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/users/{user_id}")
+async def read_item(user_id):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def get_all_users():
+    return [{"user_id1": "ruman"}, {"user_1": "user2"}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
