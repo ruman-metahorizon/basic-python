872c8d07555009b54d7327615d0fc6f5a435538b
abhishek <abhisach687@gmail.com>
2023-08-23 21:27:25 +0545

    mongo crud

--- fastapi_mongo/main.py
+++ fastapi_mongo/main.py
@@ -1,17 +1,6 @@
 from fastapi import FastAPI
+from routes.route import router
 
 app = FastAPI()
 
-from pymongo.mongo_client import MongoClient
-
-uri = "mongodb+srv://Laciann:worksmart8@cluster0.we5f2vb.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+app.include_router(router)
