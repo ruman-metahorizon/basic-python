3e6d3024406a8b400efd5cc454df0a0d0c344a8c
Carol Acharya <carolacharya@Carols-MacBook-Air.local>
2023-08-27 10:20:59 +0545

    Succesfully Connected MONGODB

--- FAST_api_class/MONGODB/schemas.py
+++ FAST_api_class/MONGODB/schemas.py
@@ -1,10 +1,11 @@
 def todo_serializer(todo) -> dict:
     return {
         'id':str(todo['_id']),
-        'name':str(todo['_name']),
-        'description':str(todo['_description']),
-        'completed':str(todo['_completed'])
+        'name':str(todo['name']),
+        'description':str(todo['description']),
+        'completed':str(todo['completed'])
     }
     
 def todos_serializer(todos) -> list:
     return [todo_serializer(todo)for todo in todos]
+
