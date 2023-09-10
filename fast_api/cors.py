7f77a641eaa73ca4fa2e59bc504519718ea99652
S0330B <sushilsharmasubedi@gmail.com>
2023-09-10 11:15:15 +0545

    login

---
+++ fast_api/cors.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+origins = [
+   "http://192.168.211.:8000",
+   "http://localhost",
+   "http://localhost:8080",
+]
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=origins,
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
+@app.get("/")
+async def main():
+   return {"message": "Hello World"}
--- fast_api/sushil/mongo_crud/models.py
+++ fast_api/sushil/mongo_crud/models.py
@@ -2,35 +2,40 @@ from typing import Optional
 
 from pydantic import BaseModel, EmailStr, Field
 
+
 class UserSchema(BaseModel):
     name: str = Field(...)
     email: EmailStr = Field(...)
     is_active: bool = Field(...)
 
+
 class UpdateUserModel(BaseModel):
-    name : Optional[str]
-    email : Optional[EmailStr]
-    is_active : Optional[bool]
-    
+    name: Optional[str]
+    email: Optional[EmailStr]
+    is_active: Optional[bool]
+
+
 class Config:
     schema_extra = {
-        "example":{
-            "name":"John Doe",
-            "email":"johndoe@gmail.com" ,
-            "is_active":True
+        "example": {
+            "name": "John Doe",
+            "email": "johndoe@gmail.com",
+            "is_active": True
+        }
     }
-    }   
-    
-def ResponseModel(data,message):
+
+
+def ResponseModel(data, message):
     return {
         "data": [data],
         "statusCode": 200,
         "message": message
     }
 
-def ErrorResponseModel(error,code,message):
+
+def ErrorResponseModel(error, code, message):
     return {
-        "error":error,
+        "error": error,
         "code": code,
         "message": message
     }
