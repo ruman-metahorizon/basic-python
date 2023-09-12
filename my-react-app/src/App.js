ece4392ca749a911e85995e0e2890de6f1a61e0a
S0330B <sushilsharmasubedi@gmail.com>
2023-09-12 11:26:12 +0545

    React

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs.js";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
-    <div className="App">
-     <h1>
-      Hello World!
-     </h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
