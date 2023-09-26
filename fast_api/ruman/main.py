cf20cd5c5204f9cdc3e4b7cfa9b89f75c2d68480
ruman <rumancha12@gmail.com>
2023-09-26 11:16:33 +0545

    fix cors issue

--- fast_api/ruman/main.py
+++ fast_api/ruman/main.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 import crud
 import models
@@ -9,6 +10,18 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency
